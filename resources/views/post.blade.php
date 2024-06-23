1f3f73bd4caad742cf6ea2b0e2066314f348f053
farhanScripts <farhan.dev06@gmail.com>
2024-06-23 15:04:20 +0700

    playlist laravel 11 (relasi)

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,8 @@
   <article class="py-8 max-w-screen-lg border-b border-gray-300">
     <h2 class="mb-1 text-3xl tracking-tight font-bold text-grey-900">{{ $post['title'] }}</h2>
     <div class="text-base text-grey-500">
-      <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
+      <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans()
+      }}
     </div>
     <p class="my-4 font-light text-justify">{{ $post['body'] }}</p>
     <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back To Posts </a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,10 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     /* :: fungsinya untuk mengambil method static dalam sebuah class */
     // $post = Post::find($post);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user}', function (User $user) {
+    return view('posts', ['title' => 'Article By ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/contact', function () {
